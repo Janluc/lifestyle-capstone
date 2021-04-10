db953bb9500d35b4db8f7c3a11e4156c1d9892ca
Janluc <Janlucsaneaux@gmail.com>
2021-04-10 19:04:13 -0400

    Delete unused files

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,10 +18,6 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
-  # GET /categories/1/edit
-  def edit
-  end
-
   # POST /categories or /categories.json
   def create
     @category = Category.new(category_params)
@@ -36,34 +32,7 @@ class CategoriesController < ApplicationController
       end
     end
   end
-
-  # PATCH/PUT /categories/1 or /categories/1.json
-  def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to @category, notice: "Category was successfully updated." }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /categories/1 or /categories/1.json
-  def destroy
-    @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find(params[:id])
-    end
 
     # Only allow a list of trusted parameters through.
     def category_params
