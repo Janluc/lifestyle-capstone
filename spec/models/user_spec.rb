cce6f38fd4d7fd198d066544be968ecb7838f853
Janluc <Janlucsaneaux@gmail.com>
2021-04-10 03:42:03 -0400

    Add unit tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'User Article creation' do
+    it 'New user should have no article' do
+      user = User.new
+      expect(user.articles.empty?).to eq(true)
+      
+    end
+
+    it "checks if user has created an article" do
+      user = User.new
+      user.save
+      user.articles.create({title: "title", text: "text"})
+      expect(user.articles.empty?).to eq(false)
+    end
+    
+  end
+end
---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Vote, type: :model do
+  context "vote ownership" do
+    it "checks if vote belongs to a article" do
+      vote = Vote.new
+      expect(vote.article_id.nil?).to eq(true)
+
+      vote2 = Vote.new({article_id: 1})
+      expect(vote2.article_id.nil?).to eq(false)
+    end
+
+    it "checks if vote belongs to a user" do
+      vote = Vote.new
+      expect(vote.user_id.nil?).to eq(true)
+
+      vote2 = Vote.new({user_id: 1})
+      expect(vote2.user_id.nil?).to eq(false)
+    end
+    
+  end
+end
