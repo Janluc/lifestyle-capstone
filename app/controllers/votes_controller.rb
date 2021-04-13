1d998e952b6984e6edbdf12ac57eb9c6cb098374
Janluc <Janlucsaneaux@gmail.com>
2021-04-13 02:29:35 -0400

    Fix voting redirection

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,7 +4,7 @@ class VotesController < ApplicationController
   def create
     @vote = current_user.votes.new(article_id: params[:article_id])
 
-    redirect_to articles_path if @vote.save
+    redirect_back(fallback_location: root_path) if @vote.save
   end
 
   # DELETE /votes/1 or /votes/1.json
@@ -12,7 +12,7 @@ class VotesController < ApplicationController
     vote = Vote.find_by(id: params[:id], user: current_user, article_id: params[:article_id])
     if vote
       vote.destroy
-      redirect_to articles_path
+      redirect_back(fallback_location: root_path)
     end
   end
 end
