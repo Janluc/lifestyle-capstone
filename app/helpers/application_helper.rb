69773b434504418f92bde799810b1b5c6e9c65b7
Janluc <Janlucsaneaux@gmail.com>
2021-05-02 00:13:10 -0400

    Fix category articles view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def vote_btn(article)
     vote = Vote.find_by(user_id: current_user.id, article_id: article.id)
     if vote
-      link_to('Vote Down', article_vote_path(id: vote.id, article_id: article.id), method: :delete)
+      link_to(' Vote Down', article_vote_path(id: vote.id, article_id: article.id), method: :delete)
     else
-      link_to('Vote Up', article_votes_path(article_id: article.id), method: :post)
+      link_to(' Vote Up', article_votes_path(article_id: article.id), method: :post)
     end
   end
 end
